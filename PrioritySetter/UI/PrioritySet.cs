e4a081abaa1b99b0d14407db44c21352f39e6cce
PatsanMCK <gleb.silver01@gmail.com>
2026-03-26 21:21:32 +0300

    Fucking around

--- PrioritySetter/UI/PrioritySet.cs
+++ PrioritySetter/UI/PrioritySet.cs
@@ -9,7 +9,7 @@ public class PrioritySet
     [UIAction("priority-set")]
     public static void SetAllPriorities()
     {
-        //Set Beat Saber to High
+        //Set Beat Saber to Hig
         Process currentProcess = Process.GetCurrentProcess();
         currentProcess.PriorityClass = ProcessPriorityClass.High;
         // Set all Discord processes needed priority
